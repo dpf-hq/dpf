c6c2ac87499444fe3a77c7f0a9cd07fa85391ae3
oyvind <oyvind@fd62162b-6573-47bd-b55f-ccae33f3af80>
2010-09-16 16:59:30 +0000

    Tidying up

--- dpf-model-editor/no.hib.dpf.metamodel/src/no/hib/dpf/metamodel/impl/EdgeImpl.java
+++ dpf-model-editor/no.hib.dpf.metamodel/src/no/hib/dpf/metamodel/impl/EdgeImpl.java
@@ -16,7 +16,6 @@ import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
-import org.eclipse.emf.ecore.impl.EObjectImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 
 /**
