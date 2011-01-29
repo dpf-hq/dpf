16e6df18c462fcedbc0e98e3a046d754f80550f4
dagvl <dagvl@fd62162b-6573-47bd-b55f-ccae33f3af80>
2011-01-29 13:53:29 +0000

    Code formatting.

--- dpf-model-editor/no.hib.dpf.editor/src/no/hib/dpf/editor/DPFPlugin.java
+++ dpf-model-editor/no.hib.dpf.editor/src/no/hib/dpf/editor/DPFPlugin.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
  * Copyright (c) 2004, 2005 Elias Volanakis and others.
-Ê* All rights reserved. This program and the accompanying materials
-Ê* are made available under the terms of the Eclipse Public License v1.0
-Ê* which accompanies this distribution, and is available at
-Ê* http://www.eclipse.org/legal/epl-v10.html
-Ê*
-Ê* Contributors:
-Ê*ÊÊÊÊElias Volanakis - initial API and implementation
-Ê*******************************************************************************/
+ï¿½* All rights reserved. This program and the accompanying materials
+ï¿½* are made available under the terms of the Eclipse Public License v1.0
+ï¿½* which accompanies this distribution, and is available at
+ï¿½* http://www.eclipse.org/legal/epl-v10.html
+ï¿½*
+ï¿½* Contributors:
+ï¿½*ï¿½ï¿½ï¿½ï¿½Elias Volanakis - initial API and implementation
+ï¿½*******************************************************************************/
 package no.hib.dpf.editor;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -18,28 +18,29 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
  * This instance can be shared between all extensions in the plugin. Information
  * shared between extensions can be persisted by using the PreferenceStore.
  * </p>
+ * 
  * @see org.eclipse.ui.plugin.AbstractUIPlugin#getPreferenceStore()
  * @author Elias Volanakis
  */
 public class DPFPlugin extends AbstractUIPlugin {
 
-/** Single plugin instance. */
-private static DPFPlugin singleton;
+	/** Single plugin instance. */
+	private static DPFPlugin singleton;
 
-/**
- * Returns the shared plugin instance.
- */
-public static DPFPlugin getDefault() {
-	return singleton;
-}
-
-/** 
- * The constructor. 
- */
-public DPFPlugin() {
-	if (singleton == null) {
-		singleton = this;
+	/**
+	 * Returns the shared plugin instance.
+	 */
+	public static DPFPlugin getDefault() {
+		return singleton;
 	}
-}
+
+	/**
+	 * The constructor.
+	 */
+	public DPFPlugin() {
+		if (singleton == null) {
+			singleton = this;
+		}
+	}
 
 }
