905745bbc3ccc8bb5f5e8cebcbce4aada0a699b4
dagvl <dagvl@fd62162b-6573-47bd-b55f-ccae33f3af80>
2011-02-02 11:05:47 +0000

    Disabled the save-code for now as it doesnt work, and save will be moved to the model anyways.

--- playground/no.hib.dpf.jython/dpf.py
+++ playground/no.hib.dpf.jython/dpf.py
@@ -1,12 +1,12 @@
 from org.eclipse.emf.ecore.resource.impl import ResourceSetImpl
-from org.eclipse.emf.ecore.xmi.impl import XMLResourceFactoryImpl
+#from org.eclipse.emf.ecore.xmi.impl import XMLResourceFactoryImpl
 from no.hib.dpf.metamodel import MetamodelFactory
 
 def save(filename, graph):
     """docstring for save"""
-    resourceSet = ResourceSetImpl()
-    resourceSet.getResrouceFactoryRegistry().getExtensionToFactoryMap()['xmi'] = XMLResourceFactoryImpl()
-   
+#    resourceSet = ResourceSetImpl()
+#    resourceSet.getResrouceFactoryRegistry().getExtensionToFactoryMap()['xmi'] = XMLResourceFactoryImpl()
+    pass
 class Graph(object):
     """Graph..."""
     def __new__(cls):
