2c405bcc74956f5a576a710b510a415612328e77
oyvind <oyvind@fd62162b-6573-47bd-b55f-ccae33f3af80>
2011-02-23 08:20:43 +0000

    Preferences page for DPF Sketcher

---
+++ dpf-model-editor/no.hib.dpf.editor/src/no/hib/dpf/editor/preferences/PreferenceConstants.java
@@ -0,0 +1,17 @@
+package no.hib.dpf.editor.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_DISPLAY_ARROWS = "displayArrowsPreference";
+//	public static final String P_PATH = "pathPreference";
+//
+//	public static final String P_BOOLEAN = "booleanPreference";
+//
+//	public static final String P_CHOICE = "choicePreference";
+//
+//	public static final String P_STRING = "stringPreference";
+	
+}
