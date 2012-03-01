4fcf46cd8af64fe9cbb3e94f8b74028851a5d95a
florian <florian@fd62162b-6573-47bd-b55f-ccae33f3af80>
2012-03-01 18:31:04 +0000

    Specification printing

--- dpf-scala-fma/no.dpf.text/src/GraphViz.scala
+++ dpf-scala-fma/no.dpf.text/src/GraphViz.scala
@@ -8,6 +8,7 @@ import scala.collection.mutable.{Set=>MSet}
 trait Output{ 
  
   private class Helper(g:AbstractGraph,name:String,path:String,printNames:Boolean=true){
+    
     def formatNode(l:List[String],n:Node,nl:Boolean):List[String]={
     	var rs=l
     	rs="\""::rs
@@ -15,7 +16,7 @@ trait Output{
     	rs=":"::rs
     	rs=getType(n)::rs
     	rs="\""::rs
-    	//With Node lable:
+    	//With Node label:
     	if(nl){
 	    	rs=" [label="::rs
 	   	  	rs="\""::rs  
@@ -25,7 +26,96 @@ trait Output{
 	   	  	rs="\"]"::rs 
     	}
     	rs
-    }		    
+    }	
+    def formatArrow(l:List[String],a:Arrow):List[String]={
+   	  var rs=l
+      rs=formatNode(rs,a.sr,false)
+   	  rs="->"::rs  
+      rs=formatNode(rs,a.tg,false)
+   	  rs=" [label="::rs
+   	  rs="\""::rs  
+   	  rs=getName(a)::rs
+      rs=":"::rs
+      rs=getType(a)::rs
+   	  rs="\"]"::rs
+   	  rs
+    }
+    def formatArrowForConstraint(l:List[String],a:Arrow):List[String]={
+   	  var rs=l
+   	  
+   	  //Add diamond:
+   	  rs="\""::rs
+   	  rs=a.id.toString::rs
+   	  rs="\""::rs
+   	  rs=" [shape=diamond,style=filled,label=\"\",height=.2,width=.2];\n"::rs
+   	  
+   	  //To diamond
+      rs=formatNode(rs,a.sr,false)
+   	  rs="->"::rs  
+   	  rs="\""::rs
+   	  rs=a.id.toString::rs
+   	  rs="\""::rs
+   	  rs=" [dir=none,weight=2];\n"::rs
+
+   	  //To target node
+   	  rs="\""::rs
+   	  rs=a.id.toString::rs
+   	  rs="\""::rs
+   	  rs="->"::rs  
+   	  rs=formatNode(rs,a.tg,false)
+   	  rs=" [label="::rs
+   	  rs="\""::rs  
+   	  rs=getName(a)::rs
+      rs=":"::rs
+      rs=getType(a)::rs
+   	  rs="\", weight=1]"::rs
+   	  rs
+    }
+    def formatConstraint(l:List[String],c:Constraint):List[String]={
+    	var rs=l
+
+    	//Add Constraint label node
+    	rs="\""::rs
+    	rs=c.s.id.toString::rs
+    	rs="\""::rs
+
+    	//With Node label:
+    	rs=" [label="::rs
+   	  	rs="\""::rs  
+    	rs=c.s.n::rs
+    	
+    	//Add Parameter
+    	rs="("::rs
+    	if(0  < c.s.p.size){
+    	  rs=c.s.p.head::rs
+	      for(p<-c.s.p.tail){
+    	   rs=", "::rs
+    	   rs=p::rs
+	      }
+    	}
+    	rs=")"::rs
+    	rs="\", shape=note, regular=1, style=filled, fillcolor=white]"::rs 
+   	  	rs=";\n"::rs 
+   	  	
+   	   //Add arrows to labelNode
+   	   var i = 0	
+   	   for(a<-c.as){
+   		   //From label node:
+   		   rs="\""::rs
+   		   rs=c.s.id.toString::rs
+    	   rs="\""::rs
+	       rs="->"::rs  
+	       //To diamond:
+	       rs="\""::rs
+	       rs=a.id.toString::rs
+	       rs="\""::rs
+		   rs=" [dir=none,label=\""::rs 
+		   rs=i.toString::rs; i+=1 //Label with arrow number in list
+		   rs="\"]; \n"::rs   	     
+   	   }	
+   	   rs
+    }	
+    
     def getName(e:Element,p:Boolean=printNames):String={
       if(p){
 	      e.id match{
@@ -88,20 +178,16 @@ trait Output{
     rs="\n{\n"::rs
     rs="graph [nodesep=\"0.7\"];\n"::rs
     rs="node [label=\"\\N\", shape=box];\n"::rs
+
+    //Nodes:
     for(n:Node<-g.nodes.values.toSet){
       rs=h.formatNode(rs,n,true)
    	  rs=";\n"::rs  
     }
+
+    //Constraints:
     for(a:Arrow<-g.arrows.values.toSet){
-      rs=h.formatNode(rs,a.sr,false)
-   	  rs="->"::rs  
-      rs=h.formatNode(rs,a.tg,false)
-   	  rs=" [label="::rs
-   	  rs="\""::rs  
-   	  rs=h.getName(a)::rs
-      rs=":"::rs
-      rs=h.getType(a)::rs
-   	  rs="\"]"::rs 
+      rs=h.formatArrow(rs,a)
    	  rs=";\n"::rs     	    
     }
     rs="}\n"::rs
@@ -124,30 +210,31 @@ trait Output{
         
     val h = new Helper(s.g,name,path,printNames)
     
-    
-    
     var rs:List[String] = Nil
     rs="digraph "::rs
     rs=name::rs
     rs="\n{\n"::rs
     rs="graph [nodesep=\"0.7\"];\n"::rs
     rs="node [label=\"\\N\", shape=box];\n"::rs
+    //Nodes:
     for(n:Node<-s.g.nodes.values.toSet){
       rs=h.formatNode(rs,n,true)
    	  rs=";\n"::rs  
     }
+    //Arrows:
     for(a:Arrow<-s.g.arrows.values.toSet){
-      rs=h.formatNode(rs,a.sr,false)
-   	  rs="->"::rs  
-      rs=h.formatNode(rs,a.tg,false)
-   	  rs=" [label="::rs
-   	  rs="\""::rs  
-   	  rs=h.getName(a)::rs
-      rs=":"::rs
-      rs=h.getType(a)::rs
-   	  rs="\"]"::rs 
+      if(arrowsWithConstraints contains a.id){
+    	rs=h.formatArrowForConstraint(rs,a)        
+      }else{
+    	rs=h.formatArrow(rs,a)
+      }
    	  rs=";\n"::rs     	    
     }
+    //Constraints:
+    for(c<-s.cs){
+      rs=h.formatConstraint(rs,c)
+    }
+    
     rs="}\n"::rs
     rs=rs.reverse 
     
