194d9ed6c0ed0a4685f4a81962da932f3246691a
anders <anders@fd62162b-6573-47bd-b55f-ccae33f3af80>
2012-04-12 14:47:16 +0000

    Moved the DPF Project specific stuff to editor project. This includes a DPF Project wizard, a DPF nature and a DPF perspective

---
+++ dpf-model-editor/trunk/plugins/no.hib.dpf.editor/src/no/hib/dpf/editor/nature/DPFProjectNature.java
@@ -0,0 +1,34 @@
+package no.hib.dpf.editor.nature;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+
+public class DPFProjectNature implements IProjectNature {
+	public static final String NATURE_ID = "no.hib.dpf.editor.nature.DPFEditorNature";
+	
+	@Override
+	public void configure() throws CoreException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deconfigure() throws CoreException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public IProject getProject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setProject(IProject project) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
