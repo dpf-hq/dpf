036c2c4dd0695f4022dd745e47721062ae98e127
ola <ola@fd62162b-6573-47bd-b55f-ccae33f3af80>
2013-04-30 10:48:37 +0000

    Fixed an override which would make nodes impossible to delete

--- dpf-model-editor/branches/no.hib.dpf.visual/src/no/hib/dpf/visualization/presentation/DPFNodeEditPart.java
+++ dpf-model-editor/branches/no.hib.dpf.visual/src/no/hib/dpf/visualization/presentation/DPFNodeEditPart.java
@@ -1,22 +1,13 @@
 package no.hib.dpf.visualization.presentation;
 
 import no.hib.dpf.editor.parts.DNodeEditPart;
-import no.hib.dpf.editor.policies.DGraphXYLayoutEditPolicy;
 import no.hib.dpf.visual.VNode;
-
-import org.eclipse.gef.EditPolicy;
 
 public class DPFNodeEditPart extends DNodeEditPart {
 
 	VNode visual = null;
 	public DPFNodeEditPart(VNode vElement) {
 		visual = vElement;
-	}
-
-	@Override
-	protected void createEditPolicies() {
-		if(visual.isComposite())
-			installEditPolicy(EditPolicy.LAYOUT_ROLE, new DGraphXYLayoutEditPolicy());
 	}
 	
 }
